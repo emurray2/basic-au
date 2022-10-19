976a6cef35f5452d3d3df42db87d8d6bae22bcc6
Evan Murray <evan@auraaudio.io>
2022-10-18 20:18:21 -0400

    Combine Models for App
    Put them all into one file

--- App/Model/ViewModels.swift
+++ App/Model/ViewModels.swift
@@ -1,6 +1,13 @@
 import SwiftUI
-import CoreMIDI
 import AudioToolbox
+
+struct AudioUnitViewModel {
+    var showAudioControls: Bool = false
+    var showMIDIContols: Bool = false
+    var title: String = "-"
+    var message: String = "No Audio Unit loaded.."
+    var viewController: ViewController?
+}
 
 class AudioUnitHostModel: ObservableObject {
     /// The playback engine used to play audio.
